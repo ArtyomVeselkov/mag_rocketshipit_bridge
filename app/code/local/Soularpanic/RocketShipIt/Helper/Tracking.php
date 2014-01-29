49950dd5e07616557c10a21d59f3a4f8ff08b48f
Joshua Kossoff <kossoff.j@gmail.com>
2014-01-29 17:54:19 -0500

    Adding tracking helper to create URLs to appropriate carrier sites.

---
+++ app/code/local/Soularpanic/RocketShipIt/Helper/Tracking.php
@@ -0,0 +1,23 @@
+<?php 
+class Soularpanic_RocketShipIt_Helper_Tracking
+extends Mage_Core_Helper_Abstract {
+  public function getTrackingUrl($track) {
+    $dataHelper = Mage::helper('rocketshipit');
+    $carrierCode = $dataHelper->parseShippingMethod($track->getCarrierCode());
+    $carrier = $carrierCode['carrier'];
+    $number = $track->getNumber();
+    switch ($carrier) {
+      case 'ups':
+	$url = "http://wwwapps.ups.com/WebTracking/track?track=yes&trackNums=$number";
+	break;
+      case 'stamps':
+	$url = "https://tools.usps.com/go/TrackConfirmAction_input?qtc_tLabels1=$number";
+	break;
+      default:
+	$url = "#";
+	break;
+    }
+    return $url;
+  }
+}
+?>
