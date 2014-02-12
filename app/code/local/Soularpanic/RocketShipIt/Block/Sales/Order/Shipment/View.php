70ef0e6b872253296e1d77e778a5af7c056b2db1
Joshua Kossoff <kossoff.j@gmail.com>
2014-02-12 12:00:55 -0500

    Fixing case on cancel shipment url.

--- app/code/local/Soularpanic/RocketShipIt/Block/Sales/Order/Shipment/View.php
+++ app/code/local/Soularpanic/RocketShipIt/Block/Sales/Order/Shipment/View.php
@@ -4,7 +4,7 @@ class Soularpanic_RocketShipIt_Block_Sales_Order_Shipment_View
 extends Mage_Adminhtml_Block_Sales_Order_Shipment_View {
   public function __construct() {
     parent::__construct();
-    $url = $this->getUrl('*/cancelshipment/cancelShipment',
+    $url = $this->getUrl('*/cancelShipment/cancelShipment',
 			array('shipmentId' => $this->getShipment()->getId()));
     $this->_addButton('cancel', array(
       'label' => 'Cancel Shipment',
